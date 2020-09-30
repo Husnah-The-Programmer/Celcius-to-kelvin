392e4b2a181d90e2687e4fce48da1954a29eca1e
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-09-30 17:25:10 +0100

    Add files via upload

---
+++ Celcius to kelvin.py
@@ -0,0 +1,5 @@
+# conversion of celcius to kelvin
+celsius = int(input("Please enter temperature in celsius:\n"))
+kelvin=(celsius+273)
+print("%0.1f degree celsius is equal to %0.1f degree kelvin"%(celsius,kelvin))
+print("celsius is",kelvin)
